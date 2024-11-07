a0c203e92283eaccdf2d1db0fdb64aec730d794c
Hugo Vega <hugo17avid@gmail.com>
2024-11-07 14:39:05 -0800

    Update EmployeeListItem.jsx

--- emplyee directory/src/routes/EmployeeListItem.jsx
+++ emplyee directory/src/routes/EmployeeListItem.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import PropTypes from 'prop-types';
 
-function EmployeeListItem() {
+function EmployeeListItem({ employee }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <li className="employee-list-item">
+      <img src={employee.picture} alt={employee.name} />
+      <div>
+        <h3>{employee.name}</h3>
+        <p>{employee.position}</p>
+      </div>
+    </li>
+  );
 }
+
+EmployeeListItem.propTypes = {
+  employee: PropTypes.shape({
+    picture: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default EmployeeListItem
